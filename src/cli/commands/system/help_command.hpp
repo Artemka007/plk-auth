d54fc8df5ce596307afa4262a47c4e232d780a05
ZenbiteX <blackender42@gmail.com>
2025-10-07 22:03:13 +0500

    feature(cli): fix help_command

--- src/cli/commands/system/help_command.hpp
+++ src/cli/commands/system/help_command.hpp
@@ -5,16 +5,10 @@
 
 class HelpCommand : public BaseCommand {
 public:
-    HelpCommand(std::string name,
-                std::string description,
-                std::shared_ptr<AppState> app_state,
-                std::shared_ptr<IOHandler> io_handler,
-                std::shared_ptr<AuthService> auth_service,
-                std::shared_ptr<UserService> user_service,
-                std::shared_ptr<LogService> log_service);
+    using BaseCommand::BaseCommand;
 
     bool execute(const std::vector<std::string> &args) override;
-    bool isVisible() const override { return true; }
+    bool isVisible() const override;
     void set_available_commands(std::unordered_map<std::string, BaseCommand *> &map);
 
 private:
